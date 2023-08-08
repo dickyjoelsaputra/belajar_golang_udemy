dd4bf8d61450b6994e5d8f48f91d9da1a6cf7478
Dicky Joel Saputra <dickyjoelsaputra@gmail.com>
2023-08-08 17:42:51 +0700

    a

---
+++ 1.Golang-Dasar/4.tipe-data-string/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Dicky")
+	fmt.Println("Dicky Joel")
+	fmt.Println("Dicky Joel Saputra")
+
+	fmt.Println(len("Dicky"))
+	fmt.Println("Dicky Joel"[0])
+}
